9a12dbe5324200cdd05fd1b017065288d14fde19
Aly El-Deen Yasser <11410120231109@stud.cu.edu.eg>
2024-11-08 13:04:52 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include "mainwindow.h"
 #include <QApplication>
 #include <QMainWindow>
+
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.setMinimumSize(1035, 725);
+    w.setMaximumSize(1035, 725);
     w.show();
     return a.exec();
 }

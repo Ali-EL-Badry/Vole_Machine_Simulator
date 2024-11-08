40f27a045d2b0aa71baebd3b92d3a158a8321ee9
Aly El-Deen Yasser <11410120231109@stud.cu.edu.eg>
2024-11-08 14:54:42 +0200

    Update Register.cpp

--- Register.cpp
+++ Register.cpp
@@ -1,10 +1,12 @@
 #include "Register.h"
+
+// Set one value in the register
 void  Register:: set_rgstr(int location,string &value)
 {
-
     rgstr[location]=value;
 }
 
+// Clear the register
 void Register:: clear()
 {
     for (int i = 0; i < 16; ++i) {
@@ -12,12 +14,13 @@ void Register:: clear()
     }
 }
 
+// Get one value in the register
 string& Register:: get_rgstr(int location)
 {
-
     return rgstr[location];
 }
 
+// return all the registers
 vector<string> Register :: print_rgstr()
 {
     return rgstr;
